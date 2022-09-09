38cd2cd980fa6165cbc85417b99c5e8c7448e638
Malcolm254 <malcolm02zm@gmail.com>
2022-09-09 13:12:26 -0700

    task 3

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,15 +2,13 @@
 
 /**
 * main - Prints the alphabetic
-*
 * Return: Always (Success)
 */
 int main(void)
 {
 	char c;
-	
 	for (c = 'a'; c <= 'z'; c++)
 	putchar(c);
 	putchar('\n');
-	return (0);
+	return(0);
 }
