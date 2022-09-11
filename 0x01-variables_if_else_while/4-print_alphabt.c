7a9eb2f01f5bdcaf149e2b4b4f234062c8c4e03c
Malcolm254 <malcolm02zm@gmail.com>
2022-09-11 12:47:14 -0700

    task 5

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,4 +1,3 @@
-:%s/\s\+$//e
 #include <stdio.h>
 
 /**
@@ -8,14 +7,14 @@
 */
 int main(void)
 {
-	char letter;
+char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-		if (letter != 'e' && letter != 'q')
-		putchar(letter);
-	}
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+	if (letter != 'e' && letter != 'q')
+	putchar(letter);
+}
 
-	putchar('\n');
-	return (0);
+putchar('\n');
+return (0);
 }
