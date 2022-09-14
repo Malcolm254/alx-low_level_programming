39b2386e32cde6e738dbf46e6cb482d7ec04aac5
Malcolm254 <malcolm02zm@gmail.com>
2022-09-14 11:35:55 -0700

    task 4-6

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,7 +14,7 @@ if (n > 0)
 	_putchar(43 + 0);
 	return (1);
 }
-else if ( n == 0)
+else if (n == 0)
 {
 	_putchar(0 + 48);
 	return (0);
--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -9,7 +9,7 @@
 int _abs(int n)
 {
 
-if ( n < 0)
-n = (-1)*n;
+if (n < 0)
+n = (-1) *n;
 	return (n);
 }
