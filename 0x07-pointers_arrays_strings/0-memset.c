a283679787dd9713d923a9b13d53a49eb2e71fb5
Malcolm254 <malcolm02zm@gmail.com>
2022-10-06 05:18:47 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,21 @@
-#include "main.h"
+#include "main.h"                                                                                                                    
+ 
+/**                                                                                                                                  
+ * _memset - Fills the first n bytes of the memory area                                                                              
+ * pointed to by @s with the constant byte @c.                                                                             
+ * @s: A pointer to the memory area to be filled.                                                                                    
+ * @c: The character to fill the memory area with.                                                                                   
+ * @n: The number of bytes to be filled.                                                                                             
+ * description _memset: over there                                                                                                   
+ * Return: A pointer to the filled memory area @s.                                                                                   
+ */                                                                                                                                  
+void *_memset(void *s, int c, size_t n)                                                                                              
+{                                                                                                                                    
 
-/**
- * _memset - fills memory with a constant byte.
- *@s: address begin to fill
- * @b: value to set on memory
- *@n: numbers of bytes to pointed by s
- * Return: char
- */
-char *_memset(char *s, char b, unsigned int n)
-{
+unsigned int index;                                                                                                          
+unsigned char *memory = s, value = c;                                                                                                                                                                                                     
+for (index = 0; index < n; index++)                                                                                          
+memory[index] = value;                                                                                               
 
-unsigned int cont = 0;
-while (cont < n)
-{
-	*(s + cont) = b;
-	cont++;
-}
-	return (s);
+return (memory);                                                                                                             
 }
