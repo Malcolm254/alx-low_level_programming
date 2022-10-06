391d637b32a1daa0982f88388a837fd67f712370
Malcolm254 <malcolm02zm@gmail.com>
2022-10-06 04:55:11 -0700

    task 0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,11 @@
 #ifndef MAIN_H                                                                                                                     
-#define MAIN_H                                                                                                                                                                                                                                                       
-#include <stddef.h>                                                                                                                
-                                                                                                                                
+#define MAIN_H                                                                                                                                                                                                                                         
+/**                                                                                                                                
+ * File: main.h                                                                                                                    
+ * Auth: sam tech (samuel birhanu)                                                                                                 
+ * description: Header file containing prototypes for all functions                                                                
+ * used in the 0x06-pointers_arrays_strings directory.                                        
+ */                                                                                                                                     
 int _putchar(char c);                                                                                                              
 void *_memset(void *s, int c, size_t n);                                                                                           
 char *_memcpy(char *dest, char *src, unsigned int n);                                                                              
